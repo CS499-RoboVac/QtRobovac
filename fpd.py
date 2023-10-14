7155c4f80a14446bd04b45d878ca594f1e4518d2
rcorob <79815272+rcorob@users.noreply.github.com>
2023-10-14 18:11:35 -0500

    Add files via upload

--- fpd.py
+++ fpd.py
@@ -5,6 +5,43 @@ import sys
 
 from ui_fpd import Ui_FPDWindow
 
+class Room:
+    def __init__(self, name, x, y, w, h, overview, fpv, combo):
+        fpv.setCurrentIndex(1)
+        self.roomName = name
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+        self.ovRoomView = QtWidgets.QFrame(overview)
+        self.ovRoomView.setGeometry(QtCore.QRect(self.x, self.y, self.width, self.height))
+        font = QtGui.QFont()
+        font.setKerning(True)
+        self.ovRoomView.setFont(font)
+        self.ovRoomView.setFrameShape(QtWidgets.QFrame.Box)
+        self.ovRoomView.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.ovRoomView.setLineWidth(1)
+        self.ovRoomView.setObjectName(name)
+        self.roomTab =  QtWidgets.QWidget()
+        fpv.addTab(self.roomTab, name)
+        self.tabRoomView = QtWidgets.QFrame(self.roomTab)
+        self.tabRoomScale = 1
+        if self.width < 600 and self.height < 480:
+            if self.width / 600 > self.height / 480:
+                self.tabRoomScale = int(600 / self.width)
+            else:
+                self.tabRoomScale = int(480 / self.height)
+        self.tabRoomView.setGeometry(QtCore.QRect(20, 20, self.width * self.tabRoomScale, self.height * self.tabRoomScale))
+        font = QtGui.QFont()
+        font.setKerning(True)
+        self.tabRoomView.setFont(font)
+        self.tabRoomView.setFrameShape(QtWidgets.QFrame.Box)
+        self.tabRoomView.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.tabRoomView.setLineWidth(1)
+        self.tabRoomView.setObjectName(name)
+        combo.addItem(name)
+        fpv.setCurrentIndex(0)
+
 class fpdWindowApp(QMainWindow, Ui_FPDWindow):
     def __init__(self, parent=None):
         super(fpdWindowApp, self).__init__(parent)
@@ -12,8 +49,7 @@ class fpdWindowApp(QMainWindow, Ui_FPDWindow):
         self.connectButtons()
         self.numRooms = 1
         self.rooms = []
-        self.room1.setObjectName("Room 1")
-        self.rooms.append(self.room1)
+        self.rooms.append(Room('Room 1', 20, 20, 120, 80, self.overviewTab, self.floorplanView, self.roomOptionsComboBox))
 
 
     def saveFloorplan(self):
@@ -26,12 +62,32 @@ class fpdWindowApp(QMainWindow, Ui_FPDWindow):
     def newFloorplan(self):
         1
 
+    def updateRoomDimensions(self):
+        for room in self.rooms:
+            if room.roomName == self.roomOptionsComboBox.currentText():
+                self.floorplanView.setCurrentIndex(1)
+                room.ovRoomView.move(int(self.roomXBox.value()), (self.roomYBox.value()))
+                room.ovRoomView.resize(int(self.roomWBox.value()), (self.roomHBox.value()))
+                self.floorplanView.setCurrentIndex(0)
+
+    def updateRoomOptions(self):
+        for room in self.rooms:
+            if room.roomName == self.roomOptionsComboBox.currentText():
+                self.roomXBox.setProperty("value", room.x)
+                self.roomYBox.setProperty("value", room.y)
+                self.roomWBox.setProperty("value", room.width)
+                self.roomHBox.setProperty("value", room.height)
+
     def addRoom(self):
         newRoomText = self.textEdit.toPlainText()
-        for i in range(0, self.numRooms):
-            if self.rooms[i].objectName() == newRoomText or newRoomText == '':
-                return
-        self.floorplanView.setCurrentIndex(1)
+        dupeFlag = False
+        for room in self.rooms:
+            if room.roomName == newRoomText or newRoomText == '':
+                dupeFlag = True
+        if not dupeFlag:
+            self.floorplanView.setCurrentIndex(1)
+            self.rooms.append(Room(newRoomText, 100, 100, 300, 180, self.overviewTab, self.floorplanView, self.roomOptionsComboBox))
+        '''
         self.r3 = QtWidgets.QFrame(self.overviewTab)
         self.r3.setGeometry(QtCore.QRect(100, 100, 301, 181))
         font = QtGui.QFont()
@@ -56,8 +112,14 @@ class fpdWindowApp(QMainWindow, Ui_FPDWindow):
         self.roomFull.setObjectName(newRoomText + "_full")
         self.floorplanView.addTab(self.room, newRoomText)
         self.rooms.append(self.room)
+        '''
         
 
     def connectButtons(self):
         self.addRoomButton.clicked.connect(self.addRoom)
         self.saveFloorplanButton.clicked.connect(self.saveFloorplan)
+        self.roomOptionsComboBox.currentIndexChanged.connect(self.updateRoomOptions)
+        self.roomXBox.valueChanged.connect(self.updateRoomDimensions)
+        self.roomYBox.valueChanged.connect(self.updateRoomDimensions)
+        self.roomWBox.valueChanged.connect(self.updateRoomDimensions)
+        self.roomHBox.valueChanged.connect(self.updateRoomDimensions)
