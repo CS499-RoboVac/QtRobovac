0c823e33c7e05e9d2d82d1e014bfb090eaf457b2
rcorob <79815272+rcorob@users.noreply.github.com>
2023-10-14 11:45:30 -0500

    Add files via upload

--- fpd.py
+++ fpd.py
@@ -10,7 +10,54 @@ class fpdWindowApp(QMainWindow, Ui_FPDWindow):
         super(fpdWindowApp, self).__init__(parent)
         self.setupUi(self)
         self.connectButtons()
+        self.numRooms = 1
+        self.rooms = []
+        self.room1.setObjectName("Room 1")
+        self.rooms.append(self.room1)
 
+
+    def saveFloorplan(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.saveFloorplanButton.setText(_translate("MainWindow", "Floorplan Saved!"))
+
+    def loadFloorplan(self):
+        1
+
+    def newFloorplan(self):
+        1
+
+    def addRoom(self):
+        newRoomText = self.textEdit.toPlainText()
+        for i in range(0, self.numRooms):
+            if self.rooms[i].objectName() == newRoomText or newRoomText == '':
+                return
+        self.floorplanView.setCurrentIndex(1)
+        self.r3 = QtWidgets.QFrame(self.overviewTab)
+        self.r3.setGeometry(QtCore.QRect(100, 100, 301, 181))
+        font = QtGui.QFont()
+        font.setKerning(True)
+        self.r3.setFont(font)
+        self.r3.setFrameShape(QtWidgets.QFrame.Box)
+        self.r3.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.r3.setLineWidth(1)
+        self.r3.setObjectName(newRoomText)
+        self.floorplanView.setCurrentIndex(0)
+
+        self.room = QtWidgets.QWidget()
+        self.room.setObjectName(newRoomText)
+        self.roomFull = QtWidgets.QFrame(self.room)
+        self.roomFull.setGeometry(QtCore.QRect(20, 20, 600, 480))
+        font = QtGui.QFont()
+        font.setKerning(True)
+        self.roomFull.setFont(font)
+        self.roomFull.setFrameShape(QtWidgets.QFrame.Box)
+        self.roomFull.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.roomFull.setLineWidth(1)
+        self.roomFull.setObjectName(newRoomText + "_full")
+        self.floorplanView.addTab(self.room, newRoomText)
+        self.rooms.append(self.room)
+        
 
     def connectButtons(self):
-        1
+        self.addRoomButton.clicked.connect(self.addRoom)
+        self.saveFloorplanButton.clicked.connect(self.saveFloorplan)
