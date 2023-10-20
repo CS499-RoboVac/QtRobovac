cc7ba6eeac8130b0d7aa942420225e9d8c585498
Mike Γ <70777578+tangentof90@users.noreply.github.com>
2023-10-20 11:34:33 -0500

    Created Sim Window in PyQt and linked windows.

--- openMain.py
+++ openMain.py
@@ -1,10 +1,21 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QLabel,
+    QVBoxLayout,
+    QWidget,
+)
 
 import sys
 
 from ui_main import Ui_MainWindow
-from fpd import fpdWindowApp
+
+# from fpd import fpdWindowApp
+# from Simulation import simWindowApp
+import fpd
+import Simulation
 
 
 class mainWindowApp(QMainWindow, Ui_MainWindow):
@@ -17,10 +28,12 @@ class mainWindowApp(QMainWindow, Ui_MainWindow):
         self.oprs = []
 
     def openFPD(self):
-        self.fpds.append(fpdWindowApp())
+        self.fpds.append(fpd.fpdWindowApp())
         self.fpds[-1].show()
 
     def openSIM(self):
+        self.sims.append(Simulation.simWindowApp())
+        self.sims[-1].show()
         1
 
     def openOPR(self):
